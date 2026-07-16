2ad605ba22a662cdfece36616f0eadcce7fd98a1
sundby <Moncherisux@mail-me.com>
2026-07-16 23:45:53 +0800

    release: prepare 0.1.5

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,8 +20,8 @@ android {
         applicationId = "dev.sundby.zhixing"
         minSdk = 26
         targetSdk = 37
-        versionCode = 5
-        versionName = "0.1.4"
+        versionCode = 6
+        versionName = "0.1.5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
