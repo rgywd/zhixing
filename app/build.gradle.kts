d0d337518bb9a1a2d38879042b4183af7b3c07eb
sundby <Moncherisux@mail-me.com>
2026-07-17 05:37:44 +0800

    release: prepare 0.1.7 persistent Codex projects

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,8 +20,8 @@ android {
         applicationId = "dev.sundby.zhixing"
         minSdk = 26
         targetSdk = 37
-        versionCode = 7
-        versionName = "0.1.6"
+        versionCode = 8
+        versionName = "0.1.7"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
