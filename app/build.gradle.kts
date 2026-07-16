b43a9cf39af194d1a316890d1f856bbc9f1e10b8
sundby <Moncherisux@mail-me.com>
2026-07-17 02:40:59 +0800

    release: prepare 0.1.6 native workflow

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,8 +20,8 @@ android {
         applicationId = "dev.sundby.zhixing"
         minSdk = 26
         targetSdk = 37
-        versionCode = 6
-        versionName = "0.1.5"
+        versionCode = 7
+        versionName = "0.1.6"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
