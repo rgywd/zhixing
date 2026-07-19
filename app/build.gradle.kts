674940d23665c65620f6e338c309a0957be3de70
sundby <Moncherisux@mail-me.com>
2026-07-19 18:31:08 +0800

    release: prepare 0.2.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,8 +24,8 @@ android {
         applicationId = "dev.sundby.zhixing"
         minSdk = 26
         targetSdk = 37
-        versionCode = 16
-        versionName = "0.2.1"
+        versionCode = 17
+        versionName = "0.2.2"
         buildConfigField("boolean", "CODEX_WORKFLOW_ENABLED", codexWorkflowEnabled.get().toString())
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
