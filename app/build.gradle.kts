0eabeebb46169226afb629cefb9c1f64a4497b92
sundby <Moncherisux@mail-me.com>
2026-07-21 11:54:26 +0800

    chore(release): prepare 0.3.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,8 +20,8 @@ android {
         applicationId = "dev.sundby.zhixing"
         minSdk = 26
         targetSdk = 37
-        versionCode = 18
-        versionName = "0.3.0"
+        versionCode = 19
+        versionName = "0.3.1"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
         ndk {
